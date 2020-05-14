7ff6f7191da958852ed3e472bd92e1061a2c131f
Crowdbotics <team@crowdbotics.com>
2020-05-14 16:09:03 +0000

    Update models

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import (
+    CustomTextViewSet,
+    HomePageViewSet,
+    R123ViewSet,
+    R456ViewSet,
+    RFFFFFViewSet,
+)
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -13,6 +20,9 @@ router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
 router.register("customtext", CustomTextViewSet)
 router.register("homepage", HomePageViewSet)
+router.register("r456", R456ViewSet)
+router.register("r123", R123ViewSet)
+router.register("rfffff", RFFFFFViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,12 @@
+from rest_framework import viewsets
+from rest_framework import authentication
+from .serializers import (
+    CustomTextSerializer,
+    HomePageSerializer,
+    R123Serializer,
+    R456Serializer,
+    RFFFFFSerializer,
+)
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAdminUser
@@ -11,7 +20,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage
+from home.models import CustomText, HomePage, R123, R456, RFFFFF
 
 
 class SignupViewSet(ModelViewSet):
@@ -49,3 +58,30 @@ class HomePageViewSet(ModelViewSet):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = [IsAdminUser]
     http_method_names = ["get", "put", "patch"]
+
+
+class R456ViewSet(viewsets.ModelViewSet):
+    serializer_class = R456Serializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = R456.objects.all()
+
+
+class R123ViewSet(viewsets.ModelViewSet):
+    serializer_class = R123Serializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = R123.objects.all()
+
+
+class RFFFFFViewSet(viewsets.ModelViewSet):
+    serializer_class = RFFFFFSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = RFFFFF.objects.all()
